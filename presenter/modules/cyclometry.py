255b7c5a46fd5ec443ae16492b2707f56975acb8
Sergei Sazonov <sergesazonov@gmail.com>
2021-10-27 12:56:03 +0300

    Cyclometry page now contains header and graph

--- presenter/modules/cyclometry.py
+++ presenter/modules/cyclometry.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 
 import gzip
 import json
+from dataclasses import dataclass, field
 from datetime import datetime, timedelta
+from typing import Optional
 
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
@@ -13,40 +15,101 @@ import pandas as pd
 from .utils.scatter_drawer2 import ScatterDrawer
 
 
+@dataclass
 class CActivity:
     """
     Represents Cyclometry Activity
     """
 
-    def __init__(self, activity_json):
-        self.activity_json = activity_json
-        self.tags = self.activity_json['RIDE']['TAGS']
-        self.samples = self.activity_json['RIDE']['SAMPLES']
-        self.identifier = self.activity_json['RIDE']['IDENTIFIER']
-        self.start_time = datetime.strptime(self.activity_json['RIDE']['STARTTIME'], '%Y/%m/%d %H:%M:%S %Z')
-        self.recording_interval = self.activity_json['RIDE']['RECINTSECS']
-        self.df = self.samples_to_df(self.samples)
-        self.duration = timedelta(seconds=int(self.df['SECS'].max()))
-        self.avg_power = int(self.df['WATTS'].mean())
-        self.avg_HR = int(self.df['HR'].mean())
-        self.max_power = int(self.df['WATTS'].max())
-        self.max_HR = int(self.df['HR'].max())
+    activity_json: dict
+    samples_key: str = 'samples'
+    time_key: str = 'secs'
+    df: pd.DataFrame = pd.DataFrame()
 
+    athlete: Optional[str] = None
+    avgHr: Optional[int] = None
+    avgPower: Optional[int] = None
+    awc: Optional[int] = None
+    awcMinValue: Optional[int] = None
+    awcs: Optional[float] = None
+    calendarText: Optional[str] = None
+    cho: Optional[int] = None
+    cp: Optional[int] = None
+    cps: Optional[float] = None
+    data: Optional[str] = None
+    device: Optional[str] = None
+    deviceInfo: Optional[str] = None
+    devicetype: Optional[str] = None
+    fat: Optional[int] = None
+    fileFormat: Optional[str] = None
+    filename: Optional[str] = None
+    gp: Optional[int] = None
+    gps: Optional[float] = None
+    id: Optional[int] = None
+    identifier: Optional[str] = None
+    maxHr: Optional[int] = None
+    maxPower: Optional[int] = None
+    notes: Optional[str] = None
+    objective: Optional[str] = None
+    recintsecs: Optional[int] = None
+    secBelowZeroAwc: Optional[int] = None
+    secBelowZeroSwc: Optional[int] = None
+    sport: Optional[str] = None
+    starttime: Optional[str] = None
+    swc: Optional[int] = None
+    swcMinValue: Optional[int] = None
+    swcs: Optional[float] = None
+    weekday: Optional[str] = None
+    workoutCode: Optional[str] = None
+    year: Optional[str] = None
 
-
-    @classmethod
-    def from_file(cls, path: str) -> CActivity:
-        with gzip.open(path, 'r') as f:
-            return cls(json.load(f))
+    def __post_init__(self):
+        self.df = self.samples_to_df(self.activity_json[self.samples_key])
+        self.populate_fields()
 
     def samples_to_df(self, samples):
-        normalized = self.normalize_samples(samples)
-        return pd.DataFrame(normalized)
+        return pd.DataFrame(samples)
+
+    def populate_fields(self):
+        for field_name, value in self.activity_json.items():
+            if field_name != 'samples_key':
+                self.__setattr__(field_name, value)
+
+    def get_activity_date_time(self):
+        try:
+            return datetime.strptime(self.starttime, '%Y/%m/%d %H:%M:%S %Z')
+        except ValueError as e:
+            return self._derive_starttime_from_identifier()
+
+    def _derive_starttime_from_identifier(self):
+        date = self.identifier.split(': ')[-1]
+        return datetime.strptime(date, '%d.%m.%Y')
 
     def normalize_samples(self, samples):
         for s, i in zip(samples, range(len(samples))):
             s['SECS'] = i
         return samples
+
+    def _list_json_fields(self):
+        s = "\'\'"
+        for k, v in self.activity_json.items():
+            if k != 'samples':
+                type_string = {type(v).__name__}
+                print(f"{k}: Optional[{type_string}] = None")
+
+    def get_activity_date_string(self):
+        return self.get_activity_date_time().strftime('%b %d, %Y')
+
+    def get_activity_duration(self):
+        return timedelta(seconds=int(self.df[self.time_key].max()))
+
+    def get_total_work(self):
+        return self.df['totalWork'].max()
+
+    @classmethod
+    def from_file(cls, path: str) -> CActivity:
+        with gzip.open(path, 'r') as f:
+            return cls(json.load(f))
 
 
 class Cyclometry:
@@ -77,40 +140,58 @@ class Cyclometry:
         return page
 
     def get_c_activity(self, activity_id: int = None) -> CActivity:
-        return CActivity.from_file('temp/2021-04-21_14-26-50.json')
+        return CActivity.from_file('temp/2021-10-21_10-16-41.json')
 
-    def make_header(self, c_activity: CActivity):
+    def make_header(self, c_activity: CActivity) -> html.Div:
+        elements = [
+            f"Activity: {c_activity.identifier}",
+            f"Date: {c_activity.get_activity_date_string()}",
+            f"Duration: {c_activity.get_activity_duration()}",
+            f"AVG Power: {c_activity.avgPower} W",
+            f"MAX Power: {c_activity.maxPower} W",
+            f"AVG HR: {c_activity.avgHr} bpm",
+            f"MAX HR: {c_activity.maxHr} bpm",
+            f"AWC below Zero: {c_activity.secBelowZeroAwc}s | {c_activity.awcMinValue}J",
+            f"SWC below Zero: {c_activity.secBelowZeroSwc}s | {c_activity.swcMinValue}J",
+            f"CP and AWC Stress: {c_activity.cps} - {c_activity.awcs}",
+            f"GP and SWC Stress: {c_activity.gps} - {c_activity.swcs}",
+            f"Total Work: {c_activity.get_total_work()} J",
+            f"CH and Fat: {c_activity.cho}g {c_activity.fat}g",
+        ]
+        first_row = html.H6(elements[0])  # style={'font-size': '0.7rem'}),
+
         header = html.Div(
             [
-                dbc.Row(dbc.Col(html.Div(f"Activity: {c_activity.identifier}"))),
-                dbc.Row(
-                    [
-                        dbc.Col(html.Div(f"Date: {c_activity.start_time.strftime('%b %m, %Y')}"), width=2),
-                        dbc.Col(html.Div(f"AVG Power: {c_activity.avg_power}"), width=2),
-                        dbc.Col(html.Div(f"AVG HR: {c_activity.avg_HR}"), width=2),
-                        dbc.Col(html.Div(f"AWC below Zero:"), width=2),
-                        dbc.Col(html.Div(f"CP and AWC Stress:"), width=2),
-                        dbc.Col(html.Div(f"Total Work:"), width=2),
-                    ]
-                ),
-                dbc.Row(
-                    [
-                        dbc.Col(html.Div(f"Duration: {c_activity.duration}"), width=2),
-                        dbc.Col(html.Div(f"MAX Power: {c_activity.max_power}"), width=2),
-                        dbc.Col(html.Div(f"MAX HR: {c_activity.max_HR}"), width=2),
-                        dbc.Col(html.Div(f"SWC below Zero"), width=2),
-                        dbc.Col(html.Div(f"GP and SC Stress"), width=2),
-                        dbc.Col(html.Div(f"CH and Fat"), width=2),
-                    ],
-                ),
-
-            ]
+                first_row,
+                self.make_row(self.make_cards(elements)),
+            ],
+            style={'font-size': '0.7rem'}
         )
-
         return header
 
-    def make_fig(self, c_activity):
-        drawer = ScatterDrawer(df=c_activity.df, index_col='SECS',
-                               series_to_plot=[s for s in c_activity.df.columns if s != 'SECS'])
+    def make_cards(self, elements: list) -> list[dbc.Card]:
+        cards = []
+
+        it = iter(elements[1:])
+        pairs = list(zip(it, it))
+        for pair in pairs:
+            card = dbc.Card(
+                dbc.CardBody(
+                    [
+                        html.P(pair[0], className="card-text"),
+                        html.P(pair[1], className="card-text"),
+                    ]
+                )
+            )
+            cards.append(card)
+        return cards
+
+    def make_row(self, cards: list[dbc.Card]):
+        row = [dbc.Col(card, width='auto') for card in cards]
+        return dbc.Row(row, no_gutters=True)
+
+    def make_fig(self, c_activity) -> ScatterDrawer.get_fig:
+        drawer = ScatterDrawer(df=c_activity.df, index_col='secs',
+                               series_to_plot=[s for s in c_activity.df.columns if s != 'secs'])
         fig = drawer.get_fig()
         return fig
--- presenter/modules/utils/scatter_drawer2.py
+++ presenter/modules/utils/scatter_drawer2.py
@@ -1,7 +1,8 @@
 #  Copyright (c) 2021. Sergei Sazonov. All Rights Reserved
 import pandas as pd
 import plotly.graph_objects as go
-
+import plotly.express as px
+from plotly.subplots import make_subplots
 from logic import Activity
 
 
@@ -28,205 +29,24 @@ class ScatterDrawer:
 
         self.figure = go.Figure()
 
-    def get_fig(self) -> go.Figure:
-        """
-        Produce figure containing require number of plots (self.series_to_plot) incl. rangeslider and
-        boxes and annotations for existing intervals
-        :return: plotly.graph_objects.Figure
-        """
-        if not self.check_fields_complete():
-            raise ValueError(f'{self.__class__.__name__} fields incomplete')
 
-        self.make_traces()
-        self.configure_traces()
-        self.configure_layouts()
-        self.update_yaxis()
 
-        self.draw_intervals()
+    def get_fig(self):
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-        # print(self.figure)
-        return self.figure
+        for chart in [ 'cad', 'hr', 'watts']:
+            fig.add_trace(
+                go.Scatter(x=self.data_frame['secs'], y=self.data_frame[chart], name=chart,  line={"width": 0.5},),
+                secondary_y=False
+            )
 
-    def make_traces(self) -> None:
-        """
-        Produce traces for all required data streams (self.series_to_plot)
-        :return: None
-        """
-        for data_stream, n in zip(self.series_to_plot, list(range(1, self.number_of_charts + 1))):
-            self.figure.add_trace(go.Scatter(
-                x=self.data_frame[self.index_col],
-                y=self.data_frame[data_stream],
-                name=data_stream,
-                yaxis=f"y{'' if n == 1 else n}"
-            ))
-        # add empty trace for interval annotations on top
-        self.figure.add_trace(go.Scatter(
-            x=[1],
-            y=[1],
-            visible=False
-        ))
+        for chart in ['awcstate', 'swcstate', 'totalWork', 'pressure']:
+            fig.add_trace(
+                go.Scatter(x=self.data_frame['secs'], y=self.data_frame[chart], name=chart,  line={"width": 0.5}),
+                secondary_y=True
+            )
 
-    def configure_traces(self) -> None:
-        """
-        Configure traces style
-        :return: None
-        """
-        self.figure.update_traces(
-            hoverinfo="name+y+text",
-            line={"width": 0.5},
-            mode="lines",
-            showlegend=False
-        )
-
-    def configure_layouts(self) -> None:
-        """
-        Configure figure layouts
-        :return: None
-        """
-        self.figure.update_layout(
-            xaxis=dict(
-                autorange=True,
-                # range=[self.data_frame.index.start, self.data_frame.index.stop],
-                rangeslider=dict(
-                    autorange=True,
-                    # range=[self.data_frame.index.start, self.data_frame.index.stop],
-                ),
-                rangeselector=dict(
-                    visible=True,
-                ),
-                type="linear"
-            ),
-            dragmode="zoom",
-            hovermode="x",
-            legend=dict(traceorder="reversed"),
-            height=600,
+        fig.update_layout(
             template="simple_white",
-            margin=dict(
-                t=0,
-                b=100
-            ),
         )
-
-    def update_yaxis(self, update_template: dict = None) -> None:
-        """
-        Update all yaxis in self.figure.layout with provided optional update
-        :param update_template: dict layout - optional.
-                If not provided - use hardcoded below todo push out to params
-        :return: None
-        """
-        if not update_template:
-            update_template = dict(
-                anchor="x",
-                autorange=True,
-                mirror=True,
-                showline=True,
-                side="right",
-                tickfont={"color": "#673ab7"},
-                tickmode="auto",
-                ticks="",
-                titlefont={"color": "#673ab7"},
-                type="linear",
-                zeroline=False
-            )
-        # Create list of relevant yaxis names
-        yaxis = {}
-        for series, i in zip(self.series_to_plot, range(1, len(self.series_to_plot) + 1)):
-            yaxis[series] = f"yaxis{i}"
-            # rename 'yaxis0' to 'yaxis' - as required by plotly
-            if yaxis[series] == "yaxis0":
-                yaxis[series] = "yaxis"
-        num_yaxis = len(yaxis)
-
-        # Produce dictionary with configuration for all yaxis
-        update = {}
-        plot_fraction = 1 / (num_yaxis + 1)
-        i = 0
-        for series, yaxis_name in yaxis.items():
-            plot_start = plot_fraction * i
-            update[yaxis_name] = dict(update_template)  # creating new dict to avoid mutation during iterations
-            # adding range and domain params
-            update[yaxis_name]['range'] = [self.data_frame[series].min(), self.data_frame[series].max()]
-            update[yaxis_name]['domain'] = [plot_start, plot_start + plot_fraction]
-            i += 1
-        self.figure.update_layout(update)
-
-    def refresh(self):
-        return self.get_fig()
-
-    def draw_intervals(self) -> None:
-        if not self.intervals:
-            return
-        shapes = []
-        annotations = []
-        for interval in self.intervals:
-            shapes.append(
-                dict(fillcolor="rgba(63, 81, 181, 0.2)",
-                     line={"width": 0},
-                     type="rect",
-                     x0=interval.start,
-                     x1=interval.end,
-                     xref="x",
-                     y0=0,
-                     y1=1,
-                     yref="paper"
-                     )
-            )
-            annotations.append(
-                dict(
-                    x=interval.start,
-                    y=1,
-                    # arrowcolor="rgba(63, 81, 181, 0.2)",
-                    # arrowsize=0.3,
-                    # ax=0,
-                    # ay=30,
-                    showarrow=False,
-                    text=interval.name,
-                    xref="x",
-                    xanchor="left",
-                    yanchor="top",
-                    yref="paper",
-                    font=dict(
-                        family="sans serif",
-                        size=6,
-                        color="crimson",
-                    ),
-                )
-            )
-
-            self.figure.update_layout(shapes=shapes, annotations=annotations)
-
-            # self.figure.add_vrect(
-            #     x0=interval.start,
-            #     x1=interval.end,
-            #     row="all",
-            #     col="all",
-            #     fillcolor="green",
-            #     opacity=0.25,
-            #     line_width=0,
-            # editable=True,
-            # edits=edits,
-            # )
-            # self.figure.add_annotation(
-            #     x=interval.start,
-            #     y=4,
-            #     valign="top",
-            #     xanchor="left",
-            #     yanchor="top",
-            #     xref="x",
-            #     yref="y",
-            #     text=f"{interval.name} <br>string2 <br>string3  <br>string4  <br>string5",
-            #     showarrow=False)
-
-    def check_fields_complete(self) -> bool:
-        """
-        :return: True if all required field of ScatterDrawer are not empty
-        (None or [], or Empty Dataframe). Otherwise - False
-        """
-        result = self.data_frame.empty or not self.index_col or not self.series_to_plot
-        return not result
-
-    def update_data_frame(self, new_data_frame: pd.DataFrame) -> None:
-        self.data_frame = new_data_frame
-
-    def update_data_series(self, new_data_series: list[str]) -> None:
-        self.series_to_plot = new_data_series
+        return fig
