0253e2a4a1976459c45af1b63297693a77ecbf5e
Sergei Sazonov <sergesazonov@gmail.com>
2021-10-28 16:17:44 +0300

    changed config to support feature flags

---
+++ presenter/cyclometry_presenter.py
@@ -0,0 +1,33 @@
+#  Copyright (c) 2021. Sergei Sazonov. All Rights Reserved
+import dash
+
+import dash_html_components as html
+
+from iobrocker import IO
+from .master_layout import MasterLayout
+from .modules import AppCalendar, CalendarFormatter, ActivityPresenter, Cyclometry
+from .base_presenter import BasePresenter
+
+
+class Presenter(BasePresenter):
+    """
+    As part of MVP pattern interacts with Model (Logic, IO) and provides formatted data to Dash View.
+    """
+    def __init__(self, view):
+        super().__init__()
+        self.view = view
+
+    def get_master_layout(self):
+        return MasterLayout().layout
+
+    def get_calendar(self) -> dash.Dash.layout:
+        context = self.view.context
+        io = IO(self.view.current_user)
+        formatter = CalendarFormatter()
+        calendar = AppCalendar(io, formatter)
+        return calendar.make_layout(context)
+
+    def get_activity(self) -> dash.Dash.layout:
+        context = self.view.context
+        io = IO(self.view.current_user)
+        return ActivityPresenter().make_layout(io, context)
